37701368cdd667324d9d62887c8ffa3c003556d6
Vladimir Andrijevik <vladimir@andrijevik.net>
2010-03-01 22:40:24 -0800

    Remove the command runner from a Worker's state.

--- lib/simpleton/worker.rb
+++ lib/simpleton/worker.rb
@@ -1,11 +1,10 @@
 module Simpleton
   class Worker
-    attr_reader :host, :middleware_chain, :command_runner
+    attr_reader :host, :middleware_chain
 
-    def initialize(host, middleware_chain, command_runner = CommandRunners::System.new)
+    def initialize(host, middleware_chain)
       @host = host
       @middleware_chain = middleware_chain
-      @command_runner = command_runner
     end
   end
 end
--- test/worker_test.rb
+++ test/worker_test.rb
@@ -1,25 +1,14 @@
 require 'test_helper'
 require 'simpleton/worker'
-require 'simpleton/command_runners/system'
 
 context "A Simpleton::Worker" do
   host = "foo"
-  middleware_chain = [Proc.new {}]
+  middleware_chain = [ Proc.new {"echo 123"} ]
 
-  context "instantiated with 3 arguments" do
-    mock_command_runner = Object.new
-    setup { Simpleton::Worker.new(host, middleware_chain, mock_command_runner) }
-
-    asserts(:host).equals(host)
-    asserts(:middleware_chain).equals(middleware_chain)
-    asserts(:command_runner).equals(mock_command_runner)
-  end
-
-  context "instantiated with 2 arguments" do
+  context "instantiated with (#{host}, #{middleware_chain.inspect})" do
     setup { Simpleton::Worker.new(host, middleware_chain) }
 
     asserts(:host).equals(host)
     asserts(:middleware_chain).equals(middleware_chain)
-    asserts(:command_runner).kind_of(Simpleton::CommandRunners::System)
   end
 end
