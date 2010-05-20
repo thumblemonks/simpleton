2a00b8de21388b615e9ca56dfd482adb92492198
Vladimir Andrijevik <vladimir@andrijevik.net>
2010-05-19 23:06:12 -0700

    Remove global Process.exit stub.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,11 +3,6 @@ require 'riot'
 require 'riot/rr'
 require 'simpleton'
 
-module Process
-  # Ensure that Workers don't exit for real in tests
-  def self.exit(*args); true; end
-end
-
 unless ENV["VERBOSE"]
   Riot.reporter = Riot::DotMatrixReporter
 end
